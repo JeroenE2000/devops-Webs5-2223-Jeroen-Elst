9bc343de654d7bbfead7d37f6c2716ec748e496b
JeroenE2000 <jmh.elst@student.avans.nl>
2023-02-13 12:25:56 +0100

    Node

---
+++ DevOps/services/database.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:27017"; 
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    family: 4
+});
+
+ 
+
+const db = client.db("mydb");
+module.exports = {
+    db: db,
+    client: client
+
+};
