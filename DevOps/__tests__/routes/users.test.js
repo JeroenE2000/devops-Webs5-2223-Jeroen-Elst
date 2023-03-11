674834bb9744ebd90476849646f5026005e38a49
JeroenE2000 <jmh.elst@student.avans.nl>
2023-03-11 14:24:22 +0100

    usertest update

--- DevOps/__tests__/routes/users.test.js
+++ DevOps/__tests__/routes/users.test.js
@@ -1,25 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const { ExpectationFailed } = require('http-errors');
 const request = require('supertest')
 const app = require('../../app')
+
 const { db, client } = require('../../services/database');
-
 describe('Get Users', () => {
-  beforeEach(async () => {
-    await db.collection('users').deleteMany({});
-  });
+    beforeEach(async () => {
+        await db.collection('users').deleteMany({});
+    });
 
-  afterAll(async() => {
-    client.close();
-  });
+    afterAll(async() => {
+        client.close();
+    });
 
-  it('should get all users in array', async () => {
-    const expected = { 'foo': 'bar' };
-    await db.collection('users').insertOne(expected);
-    delete expected._id;
-    const res = await request(app).get('/users')
-    expect(res.statusCode).toEqual(200)
-    expect(res.body.length).toEqual(1);
-    expect(res.body[0]).toEqual(expect.objectContaining(expected));
-  });
-
+    it('should get all users in array', async () => {
+        const expected = { 'foo': 'bar' };
+        await db.collection('users').insertOne(expected);
+        delete expected._id;
+        const res = await request(app).get('/users')
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.length).toEqual(1);
+        expect(res.body[0]).toEqual(expect.objectContaining(expected));
+    });
 });
